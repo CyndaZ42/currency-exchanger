7aa9d77d20e5dd5256a28165ac3624aedf9dec36
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-07-01 11:40:43 -0700

    add getElements

--- src/exchange-api.js
+++ src/exchange-api.js
@@ -1,15 +1,14 @@
 export default class Exchange {
   static getCurrency(amount, country) {
-    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${country}/${amount}`;
-    return fetch(url)
-    .then (function(response) {
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.json();
-    })
-    .catch(function(error) {
-      return error;
-    })
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${country}/${amount}`)
+      .then (function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      });
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Exchange from './exchange-api.js'
+import Exchange from './exchange-api.js';
+
+function getElements(response) {
+  if (response.result === "success") {
+    console.log(response);
+  } else {
+    console.log(response);
+  }
+}
 
 $(document).ready(function() {
-
-
-
+  $('#exchange').click(function() {
+    Exchange.getCurrency(2,"RUB")
+      .then(function(response) {
+        getElements(response);
+      });
+  });
 });
